b57d511f6d1f1ae522caf72085e7f00fdd9144d8
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-03-31 22:58:08 -0400

    made component draggable and resizable

--- src/app/components/terminal.tsx
+++ src/app/components/terminal.tsx
@@ -1,23 +1,49 @@
 'use client'
 
+import { useState, useRef } from 'react';
+import { Resizable } from 'react-resizable';
+import 'react-resizable/css/styles.css';
+import Draggable from 'react-draggable';
+
 export default function Terminal() {
-    return (
-        <div className='rounded-lg border-2 border-solid border-gray-300 flex flex-col grow h-full'>
-            <div className="bg-gray-200">
-                <div className='w-1/2 flex justify-around'>
-                    <div className='flex'>
-                        <p className='mx-2'>red</p>
-                        <p className='mx-2'>yellow</p>
-                        <p className='mx-2'>green</p>
-                    </div>
-                    <div>
-                        <p>eee</p>
-                    </div>
+  const [size, setSize] = useState({ width: 400, height: 300 });
+  const dragRef = useRef(null);
+
+  const onResize = (event, { size }) => {
+    setSize(size);
+  };
+
+  return (
+    <Draggable nodeRef={dragRef} handle=".handle">
+      <div ref={dragRef}>
+        <Resizable
+          width={size.width}
+          height={size.height}
+          onResize={onResize}
+          resizeHandles={['se']}  // Optionally add a resize handle if needed
+        >
+          <div
+            style={{ width: size.width, height: size.height }}
+            className="rounded-lg border-2 border-solid border-gray-300 flex flex-col m-4 overflow-hidden"
+          >
+            <div className="bg-gray-200 handle">
+              <div className="w-1/2 flex justify-around">
+                <div className="flex">
+                  <p className="mx-2">red</p>
+                  <p className="mx-2">yellow</p>
+                  <p className="mx-2">green</p>
                 </div>
+                <div>
+                  <p>eee</p>
+                </div>
+              </div>
             </div>
-            <div className="bg-gray-100 flex grow">
-                sadksjd
+            <div className="bg-gray-100 flex grow p-4">
+              Enter some terminal text
             </div>
-        </div>
-    );
+          </div>
+        </Resizable>
+      </div>
+    </Draggable>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Terminal from "@/app/components/terminal"
+"use client"
+import Terminal from '@/app/components/terminal'
+import Draggable from 'react-draggable';
+import { useRef } from 'react'
 
 export default function Home() {
+  const myRef = useRef(null);
+  
   return (
     <div className='h-full bg-blue-100'>
       Hello World!
+
+
       <Terminal />
+   
     </div>
   );
 }
