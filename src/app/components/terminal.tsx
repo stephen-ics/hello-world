7b83eeb10d4d800b8f95154ccaccc6a6f0e01e3f
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-03-31 22:11:44 -0400

    terminal component + layouts

---
+++ src/app/components/terminal.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+export default function Terminal() {
+    return (
+        <div className='rounded-lg border-2 border-solid border-gray-300 flex flex-col grow h-full'>
+            <div className="bg-gray-200">
+                <div className='w-1/2 flex justify-around'>
+                    <div className='flex'>
+                        <p className='mx-2'>red</p>
+                        <p className='mx-2'>yellow</p>
+                        <p className='mx-2'>green</p>
+                    </div>
+                    <div>
+                        <p>eee</p>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-100 flex grow">
+                sadksjd
+            </div>
+        </div>
+    );
+}
