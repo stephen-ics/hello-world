a9757a09040d9563a6a401a6a59296eb89461ab1
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-04-01 10:52:10 -0400

    added terminal icons

--- src/app/components/terminal.tsx
+++ src/app/components/terminal.tsx
@@ -4,6 +4,9 @@ import { useState, useRef } from 'react';
 import { Resizable } from 'react-resizable';
 import 'react-resizable/css/styles.css';
 import Draggable from 'react-draggable';
+import Image from 'next/image'
+
+import { terminal_green_hover } from 'public/terminal_green_hover'
 
 export default function Terminal() {
   const [size, setSize] = useState({ width: 400, height: 300 });
@@ -15,35 +18,52 @@ export default function Terminal() {
 
   return (
     <Draggable nodeRef={dragRef} handle=".handle">
-      <div ref={dragRef}>
-        <Resizable
-          width={size.width}
-          height={size.height}
-          onResize={onResize}
-          resizeHandles={['se']}  // Optionally add a resize handle if needed
-        >
-          <div
-            style={{ width: size.width, height: size.height }}
-            className="rounded-lg border-2 border-solid border-gray-300 flex flex-col m-4 overflow-hidden"
-          >
-            <div className="bg-gray-200 handle">
-              <div className="w-1/2 flex justify-around">
-                <div className="flex">
-                  <p className="mx-2">red</p>
-                  <p className="mx-2">yellow</p>
-                  <p className="mx-2">green</p>
+        <div ref={dragRef}>
+            <Resizable
+                width={size.width}
+                height={size.height}
+                onResize={onResize}
+            >
+                <div
+                style={{ width: size.width, height: size.height }}
+                className="rounded-lg border-2 border-solid border-gray-300 flex flex-col m-4 overflow-hidden"
+                >
+                <div className="bg-gray-300 handle hover:bg-gray-200 transition duration-300">
+                    <div className='flex'>
+                        <div className="flex w-3 m-2">
+                            <Image 
+                                src="/terminal_icons/terminal_red.png"
+                                width={500}
+                                height={500}
+                                alt="red terminal icon"
+                                className='mx-1'
+                            />
+                            <Image 
+                                src="/terminal_icons/terminal_yellow.png"
+                                width={500}
+                                height={500}
+                                alt="yellow terminal icon"
+                                className='mx-1'
+                            />
+                            <Image 
+                                src="/terminal_icons/terminal_green.png"
+                                width={500}
+                                height={500}
+                                alt="green terminal icon"
+                                className='mx-1'
+                            />
+                        </div>
+                        <div className="flex justify-center w-full">
+                            <p>📁 Stephen Ni - zsh</p>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                  <p>eee</p>
+                <div className="bg-gray-50 flex grow p-4">
+                    Enter some terminal text
                 </div>
-              </div>
-            </div>
-            <div className="bg-gray-100 flex grow p-4">
-              Enter some terminal text
-            </div>
-          </div>
-        </Resizable>
-      </div>
+                </div>
+            </Resizable>
+        </div>
     </Draggable>
   );
 }
