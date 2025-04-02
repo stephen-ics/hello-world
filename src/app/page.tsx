f1d896df12b41f69d9d0d33f5020b8552a1d5cd2
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-04-02 17:03:05 -0400

    added background wallpaper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,40 @@
 "use client"
 import Terminal from '@/app/components/terminal'
 import TerminalApp from '@/app/components/terminalApp'
+import Dock from '@/app/components/dock'
 import { useSelector } from 'react-redux'
+import { getImageProps } from 'next/image'
 
 export default function Home() {
   const terminalOpen = useSelector(state => state.application.terminalOpen)
 
+  function getBackgroundImage(srcSet = '') {
+    const imageSet = srcSet
+      .split(', ')
+      .map((str) => {
+        const [url, dpi] = str.split(' ')
+        return `url("${url}") ${dpi}`
+      })
+      .join(', ')
+    return `image-set(${imageSet})`
+  }
+
+  const {
+    props: { srcSet },
+  } = getImageProps({ alt: '', width: 1920, height: 1080, src: '/wallpaper.png' })
+  const backgroundImage = getBackgroundImage(srcSet)
+  
+  // Set the background to cover the entire viewport and maintain a nice fit
+  const style = { 
+    height: '100vh', 
+    width: '100vw', 
+    backgroundImage,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat'
+  }
   return (
-    <div className='h-screen w-screen bg-blue-100'>
+    <div className='h-screen w-screen bg-blue-100' style={style}>
       Hello World!
 
       {terminalOpen &&
@@ -15,7 +42,7 @@ export default function Home() {
       }
       
       <TerminalApp />
-   
+      <Dock />
     </div>
   );
 }
