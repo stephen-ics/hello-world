3f913ab2ee745a109f13b5926f5341c86f3f61aa
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-04-02 20:18:03 -0400

    added bounce animation for app

--- src/app/components/terminalApp.tsx
+++ src/app/components/terminalApp.tsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { openTerminal } from '../slices/applicationSlice'
-import Image from 'next/image' 
+import Image from 'next/image'
+import { motion, useAnimation } from 'framer-motion'
 
 export default function Terminal() {
     const dispatch = useDispatch()
+    const controls = useAnimation()
+
+    const handleClick = async () => {
+        await controls.start({
+            y: [0, -10, 0],
+            transition: {
+                duration: 0.6,
+                times: [0, 0.5, 1],
+                ease: 'easeOut'
+            }
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 500))
+
+        dispatch(openTerminal())
+    }
 
     return (
-        <Image
-            src='/terminal_icons/terminal_icon.png'
-            width={50}
-            height={50}
-            alt='terminal icon'
-            onClick={() => dispatch(openTerminal())}
-        >
-
-        </Image>
-    );
+        <motion.div animate={controls} onClick={handleClick}>
+            <Image
+                src='/terminal_icons/terminal_icon.png'
+                width={50}
+                height={50}
+                alt='terminal icon'
+            />
+        </motion.div>
+    )
 }
