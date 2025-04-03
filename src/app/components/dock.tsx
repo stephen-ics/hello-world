ccb1c8eaa2a8c9f0d1cba41cc4a739485f7b4cb8
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-04-02 20:08:25 -0400

    fixed bg opacity while making app opaque

--- src/app/components/dock.tsx
+++ src/app/components/dock.tsx
@@ -4,7 +4,10 @@ import TerminalApp from '../components/terminalApp'
 export default function Dock() {
     return (
         <div className='w-full flex justify-center bottom-[0.75%] absolute'>
-            <div className='w-1/2 h-[6vh] bg-gray-50 opacity-60 rounded-xl flex items-center'>
+            <div
+            className='w-1/2 h-[6vh] rounded-xl flex items-center'
+            style={{ backgroundColor: 'rgba(249, 250, 251, 0.70)' }} // gray-50 with 60% opacity
+            >
                 <div className='mx-4'>
                     <TerminalApp />
                 </div>
